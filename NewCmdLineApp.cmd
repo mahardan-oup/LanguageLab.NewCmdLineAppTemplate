548775ae1a003719c6e254453dd685b51d6e5dfb
Dan Maharry <daniel.maharry@oup.com>
2020-11-03 14:50:43 +0000

    Fix cmd shortcut to point to win-x64 dir

--- NewCmdLineApp.cmd
+++ NewCmdLineApp.cmd
@@ -4,4 +4,4 @@ if defined DOTNET_HOST_PATH (
 ) else (
     set HOST_PATH=dotnet
 )
-"%HOST_PATH%" "%~dp0\src\LanguageLab.NewCmdLineApp\bin\Debug\netcoreapp3.1\LanguageLab.NewCmdLineApp.dll" %*
+"%HOST_PATH%" "%~dp0\src\LanguageLab.NewCmdLineApp\bin\Debug\netcoreapp3.1\win-x64\NewCmdLineApp.dll" %*
